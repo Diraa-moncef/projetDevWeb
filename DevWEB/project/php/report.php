8dd15caf818874a1c8f4f0d05a6b4dae0dc03c01
ipiipi <ipiipi@pentester>
2025-01-10 00:48:00 +0000

    safezone-tester

--- DevWEB/project/php/report.php
+++
@@ -1,7 +0,0 @@
-<form id="testForm">
-    <label for="input">Test XSS:</label>
-    <input type="text" id="input">
-    <button type="submit">Test</button>
-</form>
-<div id="testResult"></div>
-
--- DevWEB/project/php/scan.php
+++
@@ -1,6 +0,0 @@
-<form id="scanForm">
-    <label for="url">Enter URL:</label>
-    <input type="text" id="url" required>
-    <button type="submit">Scan</button>
-</form>
-<div id="result"></div>
